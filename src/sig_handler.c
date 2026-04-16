444798d60c5a79793674fc3f3f497576c720db71
ArcticxFox <150258844+ArcticxFox@users.noreply.github.com>
2026-04-16 11:08:12 +0200

    Update sig_handler.c
    move signal handlers from main.c to here

--- src/sig_handler.c
+++ src/sig_handler.c
@@ -12,7 +12,25 @@
 
 #include "../header/pipex.h"
 
-int	sig_handler(void)
+void handle_sigint(int sig)
 {
+	(void)sig;
 
+	write(1, "\n", 1);
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
+}
+// cette fonction sert a gerer Ctrl+C et Ctrl+/
+void init_signals(void)
+{
+	struct sigaction sa;
+
+	sa.sa_handler = handle_sigint;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART;
+
+	sigaction(SIGINT, &sa, NULL);
+
+	signal(SIGQUIT, SIG_IGN);
 }

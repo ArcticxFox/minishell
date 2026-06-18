2551178f1941c29c84e44272efa4667b680c0fc6
ArcticxFox <elliot.joshua.jones@gmail.com>
2026-06-18 16:18:06 +0200

    j'ai mis un copie de ft_strjoin_free() dans le main pour compiler sans execute

--- src/main.c
+++ src/main.c
@@ -6,11 +6,33 @@
 /*   By: ejones <ejones.42angouleme@gmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/15 17:18:05 by ejones            #+#    #+#             */
-/*   Updated: 2026/06/18 16:09:59 by ejones           ###   ########.fr       */
+/*   Updated: 2026/06/18 16:14:54 by ejones           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/minishell.h"
+
+char	*ft_strjoin_free(char *s1, char const *s2)
+{
+	char	*s3;
+	size_t	len1;
+	size_t	lenx;
+
+	if (!s2)
+		return (NULL);
+	len1 = ft_strlen(s1);
+	lenx = (len1 + ft_strlen(s2));
+	s3 = (char *)ft_calloc((lenx + 1), sizeof(char));
+	if (!s3)
+		return (NULL);
+	if (s1)
+	{
+		ft_strlcpy(s3, s1, (len1 + 1));
+		free(s1);
+	}
+	ft_strlcpy(&s3[len1], s2, ft_strlen(s2) + 1);
+	return (s3);
+}
 
 // la fonction readline() elle return NULL quand on fait Ctrl+D,
 // line = null print exit car dans bash c'est ce qui ce passe
@@ -48,12 +70,12 @@ int	main(int ac, char **av, char **envp)
 		{
 			ft_delete_front_token(&tokens);
 		}
+		// execute(head, &shell);
+		printf("\n");
 		while(head)
 		{
 			ft_delete_front_cmd(&head);
 		}
-		execute(head, &shell);
-		printf("\n");
 		free(line);
 	}
 	return (g_value_exit);
